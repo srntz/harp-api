60599106c8aa295b29c8d40e096120bd1bfefb87
-srntz <tuzsocial909@gmail.com>
2025-07-13 16:13:07 -0600

    HRP-8 Install goose. Configure SQLC to parse Goose migrations
    - From now on, Goose is the primary source of truth for database schemas. SQLC will no longer have its own SQL schemas, it will be parsing Goose migrations and generating models from them.
    - Added migrations for artists and releases tables.

---
+++ internal/infrastructure/db/migrations/00002_add_releases_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE release_type as ENUM ('album', 'ep', 'single', 'compilation');
+
+CREATE TABLE releases (
+  id            BIGSERIAL     PRIMARY KEY,
+  created_at    TIMESTAMP     DEFAULT NOW(),
+  updated_at    TIMESTAMP     DEFAULT NOW(),
+  title         TEXT          NOT NULL,
+  release_type  release_type  NOT NULL,
+  image_url     TEXT CHECK (image_url SIMILAR TO 'https?://%'),
+  artist_id     UUID     REFERENCES artists(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE releases;
+DROP TYPE release_type;
+-- +goose StatementEnd
